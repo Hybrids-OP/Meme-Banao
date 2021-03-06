a73ca0341981a15e83956eb1d4c65c8d7e117db4
Rishabh Purohit <rishabh.purohit123456789@gmail.com>
2021-03-06 22:30:13 +0530

    created NavBar

---
+++ client/src/components/home/home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Card, CardContent, CardMedia, Typography } from '@material-ui/core';
+import homepageimage from '../../images/backgroung.jpg';
+import useStyles from './style'
+
+const Home = () => {
+    const classes = useStyles(); 
+    return(
+        <Card className={classes.card}  >
+            <Typography className={classes.title} variant="h6">HOME PAGE </Typography>
+            <CardMedia  className={classes.media} image={homepageimage} title="HomePage"></CardMedia>
+            <CardContent>
+                <Typography variant="body2" component="p">
+                    Welcome Welcome!
+                </Typography>
+            </CardContent>
+        </Card>
+        
+    );
+}
+
+
+export default Home;
---
+++ client/src/components/home/style.js
@@ -0,0 +1,17 @@
+import {makeStyles} from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+    card:{
+        maxWidth: 600,
+        margin: 'auto',
+        marginTop: theme.spacing(5)
+    },
+    title:{
+        padding: `${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(2)}px`,
+        color:'#0b090a'
+    },
+    media:{
+        minHeight: 400
+    }
+
+}))
