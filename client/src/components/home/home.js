3bde5dc7a1739eb344c908e842a7bdfbe9b1f714
Rishabh Purohit <rishabh.purohit123456789@gmail.com>
2021-03-10 21:25:42 +0530

    tiny updates

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -7,13 +7,13 @@ const Home = () => {
     const classes = useStyles(); 
     return(
         <Card className={classes.card}  >
-            <Typography className={classes.title} variant="h6">HOME PAGE </Typography>
+            <Typography className={classes.title} variant="h6">Surfing and CHILL!</Typography>
             <CardMedia  className={classes.media} image={homepageimage} title="HomePage"></CardMedia>
-            <CardContent>
+            {/* <CardContent>
                 <Typography variant="body2" component="p">
                     Welcome Welcome!
                 </Typography>
-            </CardContent>
+            </CardContent> */}
         </Card>
         
     );
