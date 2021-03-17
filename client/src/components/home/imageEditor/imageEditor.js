003ad7bf9cd1184def67c98d2caee0597765cea9
Rishabh Purohit <rishabh.purohit123456789@gmail.com>
2021-03-17 17:23:24 +0530

    changed styles

--- client/src/components/home/imageEditor/imageEditor.js
+++ client/src/components/home/imageEditor/imageEditor.js
@@ -2,16 +2,28 @@ import React from 'react';
 import useStyles from './style';
 import {Card, Typography, Button, TextField, Container, Grid} from '@material-ui/core';
 import homepageimage from '../../../images/backgroung.jpg'
-
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
 
 const ImageEditor = () => {
     const classes = useStyles();
     return(
         <>
         <Card className={classes.card}  >
-            <Button variant="contained" color="secondary" fullWidth >
-                <Typography>Upload Template</Typography>
-            </Button>
+                <input
+                    accept="image/*"
+                    className={classes.input}
+                    id="contained-button-file"
+                    multiple
+                    type="file"
+                />
+                <label htmlFor="contained-button-file">
+                    <Button variant="contained" color="secondary" component="span" fullWidth startIcon={<CloudUploadIcon style={{ fontSize: 70, margin:60 }} />} >
+                        <Typography className={classes.newtempbtn}>Upload New Template</Typography>
+                    </Button>
+                </label>
             <TextField
                 className={classes.textf} 
                 name='message'
@@ -21,8 +33,14 @@ const ImageEditor = () => {
                 />
         </Card>
         <br/>
-        <Container>
-            <Grid container className={classes.container} >
+        <Container >
+            <Button className={classes.submitButton} variant="contained" size="large" color="primary" >
+                <Typography className={classes.submitButton}>G0!</Typography>
+            </Button>
+            <IconButton aria-label="delete"  size="large">
+                <DeleteIcon style={{ fontSize: 70, margin:60 }}/>
+            </IconButton>
+            {/* <Grid container className={classes.container} >
                 <Grid xs={12} sm={6}>
                     <Button className={classes.submitButton} variant="contained" color="primary"  >
                         <Typography>GO!</Typography>
@@ -33,7 +51,7 @@ const ImageEditor = () => {
                         <Typography>Reset</Typography>
                     </Button>
                 </Grid>
-            </Grid>
+            </Grid> */}
         </Container>
         </>
 
